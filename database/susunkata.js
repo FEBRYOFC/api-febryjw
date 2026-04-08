82bbf2cf75066896dc2edcfe18b4b93728208573
FEBRYOFC <peler1101010@gmail.com>
2026-04-08 23:49:00 +0700

    Add files via upload

---
+++ database/susunkata.js
@@ -0,0 +1,12 @@
+[
+  {"soal": "U-B-K-U", "jawaban": "BUKU", "tipe": "Benda"},
+  {"soal": "M-U-R-A-J", "jawaban": "JARUM", "tipe": "Benda"},
+  {"soal": "G-N-A-G-J-U", "jawaban": "JAGUNG", "tipe": "Tumbuhan"},
+  {"soal": "U-R-G-U", "jawaban": "GURU", "tipe": "Pekerjaan"},
+  {"soal": "O-N-L-E-M", "jawaban": "MELON", "tipe": "Buah"},
+  {"soal": "R-G-U-N-K-A", "jawaban": "KARUNG", "tipe": "Benda"},
+  {"soal": "N-U-B-S-A", "jawaban": "SABUN", "tipe": "Benda"},
+  {"soal": "T-A-R-S-E-K", "jawaban": "KERTAS", "tipe": "Benda"},
+  {"soal": "R-O-M-O-T", "jawaban": "MOTOR", "tipe": "Benda"},
+  {"soal": "M-P-A-S-N-A", "jawaban": "SAMPAN", "tipe": "Benda"}
+]
